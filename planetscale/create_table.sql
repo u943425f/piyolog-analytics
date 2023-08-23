5ca912e771337f07148332e8c2e35019ee592199
Kento <u943425f@gmail.com>
2023-08-23 21:42:30 +0900

    対象データを変更

--- planetscale/create_table.sql
+++ planetscale/create_table.sql
@@ -1,17 +1,9 @@
 CREATE TABLE stats (
   id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
   date varchar(255) UNIQUE NOT NULL,
-  nap_count INT UNSIGNED,
-  nap_minute FLOAT UNSIGNED,
-  night_sleep_count INT UNSIGNED,
-  night_sleep_minute FLOAT UNSIGNED,
-  night_wakeup_count INT UNSIGNED,
-  night_wakeup_minute FLOAT UNSIGNED,
   milk_count INT UNSIGNED,
   milk_ml FLOAT UNSIGNED,
-  breastfeeding_count INT UNSIGNED,
-  breastfeeding_minute FLOAT UNSIGNED,
-  baby_food_count INT UNSIGNED,
-  baby_food_minute FLOAT UNSIGNED,
+  unchi_count INT UNSIGNED,
+  unchi_amount INT UNSIGNED,
   age_of_month INT UNSIGNED
 );
